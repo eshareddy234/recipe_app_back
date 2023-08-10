4daa7e23fc3bcc233571dd4fb90f58f248249f13
eshareddy234 <119891063+eshareddy234@users.noreply.github.com>
2023-08-10 16:57:26 +0530

    Add files via upload

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+mongoose.set('strictQuery', true);
+
+mongoose
+    .connect(process.env.MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },(err)=>{
+    if(err){
+      console.log("errorrrrr")
+      console.log(err)
+    }
+    else{
+      console.log("successfully connected")
+    }
+
+  });
+
+app.listen(PORT, () => console.log("Server started on",PORT));
---
+++ server/src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+mongodb+srv://eshareddy234:eshareddy234@recipes.ybbrgcj.mongodb.net/recipes?retryWrites=true&w=majority
